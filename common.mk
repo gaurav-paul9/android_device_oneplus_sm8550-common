3111127f20dd64bec116d1f4effbbce312f89990
gaurav-paul9 <paulgaurav9@gmail.com>
2025-11-23 17:45:58 +0000

    Revert "sm8550-common: Adapt powerhint stuff to los"
    This reverts commit ea2e39ec16587eef4593a44c1e679c9639dd24ef.

--- common.mk
+++ common.mk
@@ -320,7 +320,7 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/interfaces \
+    hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/oplus \
